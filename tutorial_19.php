3a8e825f15dc3397769a38483ed088e1b4df26d7
Won Lee <wonseob2207@gmail.com>
2025-03-18 12:41:37 -0600

    Add files via upload

---
+++ tutorial_19.php
@@ -0,0 +1,24 @@
+<?php
+
+    include("header.html");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>This is the HOME Page </p> <br>
+    <p>Stuff about your HOME page can go here </p> 
+</body>
+</html>
+
+<?php
+
+    include("footer.html");
+
+?>
