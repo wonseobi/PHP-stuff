1ff86ecd968a50dc5cb4a70c9f496289b46fea12
Won Lee <wonseob2207@gmail.com>
2025-03-19 10:32:21 -0600

    Add files via upload

---
+++ tutorial_21_2.php
@@ -0,0 +1,32 @@
+<?php
+
+    session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    This is the home page <br>
+    <form action="tutorial_21_2.php" method="post">
+        <input type="submit" name="logout" value="logout">
+    </form>
+</body>
+</html>
+
+<?php
+
+    echo $_SESSION["username"] . "<br>";
+    echo $_SESSION["password"] . "<br>";
+
+    if(isset($_POST["logout"])){
+        session_destroy();
+        header("Location: index.php");
+    }
+
+?>
